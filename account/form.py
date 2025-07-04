5f6149f338c1c9432af4518bbd556b50c9dc4372
Jayesh <102299348+Jayyesh26@users.noreply.github.com>
2025-07-04 13:15:43 +0530

    form.py

--- account/form.py
+++ account/form.py
@@ -1,6 +1,6 @@
 from dataclasses import fields
 from pyexpat import model
-from tkinter import Widget
+# from tkinter import Widget
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
